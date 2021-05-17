223ce21ef25a3f254af81c4200bb4f4b000c94f0
Fondem Junior <fondemjunior@yahoo.com>
2021-05-17 14:45:32 +0100

    Add modify test samples

--- sample.rb
+++ sample.rb
@@ -1,21 +1,22 @@
-#!/usr/bin/env ruby
 class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
   
-    def initialize(name, age)
-      @name = name
-      @age = age
-    end.
-    
-    def about_me
-      puts "I'm #{@name} and I'm #{@age} years old!"
-    end
-    
-    def bank_account_number
-      @account_number = 12345
-      puts "My bank account number is #{@account_number}."
-    end
+
+  def about_me 
+    puts "I'm #{@name} and I'm #{@age} years old!"
+   end
+
+  
+
+  def bank_account_number
+
+    @account_number = 12_345
+    puts "My bank account number is #{@account_number}."
   end
-  
-  eric = Person.new("Eric", 26)
-  eric.about_me
-  eric.bank_account_number
+end
+
+eric = Person.new('Eric', 26)
+eric.about_me
+eric.bank_account_number
