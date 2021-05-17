7433fd2b07371acf6f1de58b1e62628811393485
Fondem Junior <fondemjunior@yahoo.com>
2021-05-17 10:19:18 +0100

    Add the filereader

---
+++ lib/read_file.rb
@@ -0,0 +1,17 @@
+require 'colorize'
+
+class FileReader
+  attr_reader :file_path, :lines_count, :file_lines, :error_message
+
+  def initialize(file_path)
+    @file_path = file_path
+    @error_message = ' '
+    begin
+      @file_lines = File.readlines(@file_path)
+      @lines_count = @file_lines.size
+    rescue StandardError => e
+      @file_lines = []
+      @error_message = "Incorrect file path, correct your file path and try again\n".colorize(:red)
+    end
+  end
+end
